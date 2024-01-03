cfc110dac1444331cbf4cedce5f8240e7a817348
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-03 18:59:25 +0200

    Edit task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@ export default function handleResponseFromAPI(promise) {
     status: 200,
     body: 'success',
   })).catch(() => {
-    throw new Error('');
+    return new Error('');
   }).finally(() => {
     console.log('Got a response from the API');
   });

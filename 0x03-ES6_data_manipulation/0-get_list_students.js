4ac559d4c7ea8c43afaef815bb2e244b5dc5974a
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-08 20:02:59 +0200

    Fix loaction for index 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 const createObj = (id, firstName, location) => ({ id, firstName, location });
 
 const getListStudents = () => [
-  createObj(1, 'Guillaume', 'Francisco'),
+  createObj(1, 'Guillaume', 'San Francisco'),
   createObj(2, 'James', 'Columbia'),
   createObj(5, 'Serena', 'San Francisco'),
 ];

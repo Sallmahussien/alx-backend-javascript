2132b928bef586b9335d0e2386a146c25fb40af5
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-08 20:00:02 +0200

    Fix eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,9 @@
-const getListStudents = () => {
-    return [
-        createObj(1, 'Guillaume', 'Francisco'),
-        createObj(2, 'James', 'Columbia'),
-        createObj(5, 'Serena', 'San Francisco')
-    ];
-}
+const createObj = (id, firstName, location) => ({ id, firstName, location });
 
-const createObj = (id, firstName, location) => {
-    return { id, firstName, location };
-}
+const getListStudents = () => [
+  createObj(1, 'Guillaume', 'Francisco'),
+  createObj(2, 'James', 'Columbia'),
+  createObj(5, 'Serena', 'San Francisco'),
+];
 
 export default getListStudents;

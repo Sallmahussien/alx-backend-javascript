1edf6716879339de0fbfbabcc6210e5d92ae6d25
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-27 02:54:40 +0200

    Fix task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,12 @@ const http = require('http');
 const portNumber = 1245;
 const hostname = 'localhost';
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(portNumber, hostname, () => {
+app.listen(portNumber, hostname, () => {
   console.log(`Server running at http://${hostname}:${portNumber}/`);
 });
